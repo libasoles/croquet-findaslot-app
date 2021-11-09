a1927d20ed14f68b94b2b71eaceb28167081e929
Guillermo <gperez78@gmail.com>
2021-11-09 09:32:58 -0300

    Build index based on locale

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
-const locale =
-  window.location.pathname === "/en"
-    ? {
-        lang: "en",
-        locale: "en-US",
-      }
-    : {
-        lang: "es",
-        locale: "es-ES",
-      };
+const locale = window.location.pathname.includes("en")
+  ? {
+      lang: "en",
+      locale: "en-US",
+    }
+  : {
+      lang: "es",
+      locale: "es-ES",
+    };
 
 export const config = {
   ...locale,
