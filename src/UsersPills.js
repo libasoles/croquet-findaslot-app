527e78228da0a0ff8bae324fcd6ac561c75d3a56
Guillermo <gperez78@gmail.com>
2021-11-10 15:58:13 -0300

    select self pill by default

--- src/UsersPills.js
+++ src/UsersPills.js
@@ -4,29 +4,49 @@ import { render } from "@itsjavi/jsx-runtime/src/jsx-runtime/index";
 export class PillsView extends View {
   constructor(identity) {
     super(identity);
-
     this.identity = identity;
 
+    this.subscribe("identity", "established", this.init);
+
     // this.subscribe("calendar", "selected-slots", this.render);
+  }
+
+  init() {
+    this.initialState();
 
     this.render();
   }
 
-  render() {
-    const toggle = (e) => {
-      const element = e.currentTarget;
-      element.classList.toggle("selected");
-    };
+  initialState() {
+    this.selected = [this.identity.selfId(this.viewId)];
+  }
 
+  render() {
     // TODO: display only the ones who actually selected something
-    const pills = this.identity.allUsers().map((user, index) => {
+    const toPill = (user, index) => {
+      const isSelected = this.selected.includes(user.userId);
+      const isChecked = false;
+
       return (
-        <button className="pill" onClick={toggle}>
+        <button
+          className={`pill ${isSelected && "selected"} ${
+            isChecked && "checked"
+          }`}
+          onClick={this.toggle.bind(this)}
+        >
           {user.userName || "Anonymous #" + (index + 1)}
         </button>
       );
-    });
+    };
+
+    const pills = this.identity.allUsers().map(toPill);
 
     render(<>{pills}</>, document.querySelector(".users-pills"));
+  }
+
+  toggle(event) {
+    event.currentTarget.classList.toggle("selected");
+
+    this.subscribe("calendar", "display-users-selection", {});
   }
 }
